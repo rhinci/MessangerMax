f880290aaae3d1e9134415f3bcb998ca933a861b
rhinci <dashalupanova@gmail.com>
2025-11-13 13:15:51 +1000

    class server fields and without methods

--- ChatServer/Services/Server.cs
+++ ChatServer/Services/Server.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
+using ChatCommon.Models;
 
 namespace ChatServer.Services
 {
-    internal class Server
+    public class Server
     {
+        private TcpListener? _listener;
+        private List<ClientHandler> _clients;
+        private bool _isRunning;
+
+
+        public Server()
+        {
+            _clients = new List<ClientHandler>();
+            _isRunning = false;
+        }
+
+
+        public bool IsRunning
+        {
+            get { return _isRunning; }
+        }
+
+
+
+        public void Start(int port)
+        {
+            // запуск сервера
+        }
+
+        public void Stop()
+        {
+            // остановка сервера
+        }
+
+        private async Task AcceptClientsAsync()
+        {
+            // принятие подключений
+        }
     }
 }
