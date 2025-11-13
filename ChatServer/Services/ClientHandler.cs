3782c77df06562fc156fa1abe9b66a83789ed534
rhinci <dashalupanova@gmail.com>
2025-11-13 23:04:55 +1000

    ClientHandler class without methods

--- ChatServer/Services/ClientHandler.cs
+++ ChatServer/Services/ClientHandler.cs
@@ -1,12 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Sockets;
 using System.Threading.Tasks;
+using ChatCommon.Models;
 
 namespace ChatServer.Services
 {
-    internal class ClientHandler
+    public class ClientHandler
     {
+        private readonly TcpClient _tcpClient;
+        private readonly Server _server;
+        private NetworkStream? _stream;
+        private string _clientName;
+
+        public ClientHandler(TcpClient tcpClient, Server server)
+        {
+            _tcpClient = tcpClient;
+            _server = server;
+            _clientName = "Unknown"; // временно
+        }
+
+        public string ClientName
+        {
+            get { return _clientName; }
+        }
+
+        public bool IsConnected
+        {
+            get { return _tcpClient?.Connected ?? false; }
+        }
+
+        public async Task ListenToClientAsync()
+        {
+            // слушать сообщения от клиента
+        }
+
+        public void SendMessage(Message msg)
+        {
+            // отправить сообщение клиенту
+        }
+
+        public void Disconnect()
+        {
+            // отключить клиента
+        }
     }
 }
