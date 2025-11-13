bc0bc2b41b61f8c4c125108ca99abcb49a50f4c5
rhinci <dashalupanova@gmail.com>
2025-11-13 12:57:25 +1000

    complete message class

--- ChatCommon/Models/Message.cs
+++ ChatCommon/Models/Message.cs
@@ -3,10 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ChatCommon.Models
 {
-    internal class Message
+    public class Message
     {
+        private string? _sender;
+        private string? _text;
+        private DateTime _timestamp;
+        private string? _receiver;
+        private byte[]? _fileData;
+        private string? _fileName;
+
+        public string? Sender
+        {
+            get { return _sender; }
+            set { _sender = value; }
+        }
+
+        public string? Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+        public DateTime Timestamp
+        {
+            get { return _timestamp; }
+            set { _timestamp = value; }
+        }
+
+        public string? Receiver
+        {
+            get { return _receiver; }
+            set { _receiver = value; }
+        }
+
+        public byte[]? FileData
+        {
+            get { return _fileData; }
+            set { _fileData = value; }
+        }
+
+        public string? FileName
+        {
+            get { return _fileName; }
+            set { _fileName = value; }
+        }
+
+
+        // упаковка письма в JSON
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        // распаковка JSON в письмо
+        public static Message? FromJson(string json)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<Message>(json);
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
+
